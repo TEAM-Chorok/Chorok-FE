f0cf663914f1b004bad802246f7a7e6d32df0cc1
isabel-noh <nohhyunjeong93@gmail.com>
2022-05-28 22:19:14 +0900

    window.alert 수정

--- src/Pages/Community/AddPost.jsx
+++ src/Pages/Community/AddPost.jsx
@@ -68,7 +68,7 @@ const AddPost = () => {
                     <Button type="filter" checked={category === "postType04"} _onClick={() => {setCategory("postType04")}}>식물추천</Button>
                 </Grid>
                 <Grid padding="0 16px" width="100%">
-                    <Input maxLength="15" type="text" placeholder='글 제목을 입력해주세요'
+                    <Input maxLength="25" type="text" placeholder='글 제목을 입력해주세요'
                     onChange={(e) => {setPostTitle(e.target.value)}}></Input>
                 </Grid>
                 <Grid width="100%" padding="16px">
--- src/Pages/Community/EditPost.jsx
+++ src/Pages/Community/EditPost.jsx
@@ -111,7 +111,7 @@ const EditPost = () => {
                         _onClick={() => {setCategory("postType04"); setPostTypeCode("postType04")}}>식물추천</Button>
                 </Grid>
                 <Grid padding="0 16px" width="100%">
-                    <Input maxLength="15" type="text" placeholder='글 제목을 입력해주세요' defaultValue={post?.postTitle}
+                    <Input maxLength="25" type="text" placeholder='글 제목을 입력해주세요' defaultValue={post?.postTitle}
                     onChange={(e) => {setPostTitle(e.target.value)}}></Input>
                 </Grid>
                 <Grid width="100%" padding="16px">
