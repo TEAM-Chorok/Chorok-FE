f490a8bbae2dc97784028eb7c801b784165bcac4
isabel-noh <nohhyunjeong93@gmail.com>
2022-05-30 15:50:02 +0900

    주석 정리

--- src/Pages/Community/AddPost.jsx
+++ src/Pages/Community/AddPost.jsx
@@ -28,32 +28,6 @@ const AddPost = () => {
         });
     };
 
-    //이미지 re-sizing 
-    // const handleFileOnChange = async (e) => {
-    //     let file = e.target.files[0];	// 입력받은 file객체
-
-    //     // 이미지 resize 옵션 설정 (최대 width을 100px로 지정)
-    //     const options = { 
-    //         maxSizeMB: 2, 
-    //         maxWidthOrHeight: 100
-    //     }
-
-    //     try {
-    //       const compressedFile = await imageCompression(file, options);
-    //       setFile(compressedFile);
-
-    //       // resize된 이미지의 url을 받아 fileUrl에 저장
-    //       const promise = imageCompression.getDataUrlFromFile(compressedFile);
-    //       promise.then(result => {
-    //           setFileUrl(result);
-    //       })
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    //   }
-
-
-
     const submit = () => {
         dispatch(postActions.addPostDB(postTitle, imageUrl, postContent, category));
     }
